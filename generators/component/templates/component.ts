655744a442e98e3fefae9f0ffeb7b3aa71c21e56
Benjamin Charity <ben@benjamincharity.com>
2018-01-04 11:47:12 -0500

    fix(Component): update format, comments and add demo link

--- generators/component/templates/component.ts
+++ generators/component/templates/component.ts
@@ -2,21 +2,34 @@ import {
   Component,
   Input,
   Output,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  ViewEncapsulation,
 } from '@angular/core';
 
 /**
  * TODO: Fill this section out
  * This is the <%= kebabName %> UI Component
  *
+ * #### QA CSS CLASSES
+ * - `qa-<%= kebabName %>`: Placed on the primary container
+ *
  * @example
  * <ts-<%= kebabName %>
  *              item="Value"
  * ></ts-<%= kebabName %>>
+ *
+ * <example-url>https://goo.gl/ieUPaG</example-url>
  */
 @Component({
   selector: 'ts-<%= kebabName %>',
   templateUrl: './<%= kebabName %>.component.html',
   styleUrls: ['./<%= kebabName %>.component.scss'],
+  host: {
+    class: 'ts-<%= kebabName %>',
+  },
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class <%= componentName %> {
 
