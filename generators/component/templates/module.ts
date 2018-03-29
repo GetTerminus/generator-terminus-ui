77be5d47816afe62163846ecbe07131b59bef457
Benjamin Charity <ben@benjamincharity.com>
2018-03-29 10:52:52 -0400

    fix: module now exports everything from component

--- generators/component/templates/module.ts
+++ generators/component/templates/module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { <%= componentName %> } from './<%= kebabName %>.component';
-export { <%= componentName %> } from './<%= kebabName %>.component';
+export * from './<%= kebabName %>.component';
 
 
 @NgModule({
