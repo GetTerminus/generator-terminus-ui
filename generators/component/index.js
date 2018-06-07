c8d01f90d2db2fd2da6793a39b21e812adf4a0e9
Benjamin Charity <ben@benjamincharity.com>
2018-06-07 09:03:06 -0400

    fix: add missing pascal name definition

--- generators/component/index.js
+++ generators/component/index.js
@@ -100,6 +100,7 @@ module.exports = class extends Generator {
           {
             kebabName: this.options.name,
             componentName: this.options.componentName,
+            pascalName: this.options.pascalName,
           }
         );
 
